fb769f4c999f576015da3151e6fe67a2fdaab3c1
paganprey <nafy141625@gmail.com>
2019-12-26 13:25:16 +0530

    fix

--- push.js
+++ push.js
@@ -7,6 +7,9 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto: nafy141625@gmail.com', vapidKeys.publicKey , vapidKeys.privateKey)
 
-let sub = {};
-
+let sub = {
+	     "endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABeBGJUtd6WMoOgNAKgud6WUTv_CdagtCklz2Y8injhtBNo7HnYv3JbSna0ETfLeSyDmWQPblIxECzfiSbRnEsopfl2soN2mKC6-D_iaMmj1ai82sqU6ciAAIrAkdc4UIGoVVVPS6PXzIomL0J-q-Qk1J2IpReb7HyO342qkn1pG3YYIqY",
+         "keys":{"auth":"EX6Q3EQKI5-q-SfeGdT9ew",
+         "p256dh":"BH3ghb-FdSLiAE-GdU7VQ2MA16SKywADeHnqDVWoW4GBTcjPSzCcoHoW48Q88reX2W3pQsCAa2JYIJ-0pJdpvPk"
+     }}
 push.sendNotification(sub, 'Test Message')
